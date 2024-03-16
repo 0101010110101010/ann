fdbc2ae238c1b1d5ff3b275a2deea4872a789006
zx <1234321aaaaaa@sina.com>
2024-03-16 14:27:04 +0800

    add muti hide code

--- test.c
+++ test.c
@@ -5,13 +5,14 @@
 
 #define INPUT_NODE  2 // input  neuron num
 #define HIDE_NODE   4 // hide   neuron num
+#define HIDE_NODE_COL   2 // hide   neuron num
 #define OUTPUT_NODE 1 // output neuron num
 
 #define MAX_NUM 300
 
-double studyRate = 0.8;  //study rate
+double studyRate = 1.8;  //study rate
 double threshold = 1e-4; //max mistake
-double mostTimes = 1e6; //max study times 
+double mostTimes = 1e10; //max study times 
 double trainSize = 0; 
 double testSize = 0; 
 
@@ -32,7 +33,7 @@ typedef struct Node{
 }Node;
 
 Node inpuLayer[INPUT_NODE];
-Node hideLayer[HIDE_NODE];
+Node hideLayer[HIDE_NODE][HIDE_NODE_COL];
 Node outputLayer[OUTPUT_NODE];
 
 double sigmoid(double x)
@@ -145,7 +146,7 @@ void printData(Sample * data, int size)
 
 void init()
 {
-	int i,j;
+	int i,j,k;
 	srand(time(0));
 
 	//input init
@@ -157,22 +158,41 @@ void init()
 		inpuLayer[i].bias_delta = 0.0;
 		for(j = 0; j< HIDE_NODE; j++)
 		{
-			inpuLayer[i].weight[j] = rand() % 10000 / (double)10000 * 2 - 1;
+			//inpuLayer[i].weight[j] = rand() % 10000 / (double)10000 * 2 - 1;
+			inpuLayer[i].weight[j] = 1;
 			inpuLayer[i].weight_delta[j] = 0.0;
+		}
+	}
+
+	for(i = 0; i< HIDE_NODE; i++)
+	{
+		for(j = 0; j< HIDE_NODE_COL - 1; j++)
+		{
+			hideLayer[i][j].weight = malloc(sizeof(double) * HIDE_NODE);
+			hideLayer[i][j].weight_delta = malloc(sizeof(double) * HIDE_NODE);
+			hideLayer[i][j].bias = rand() % 10000 / (double)10000 * 2 - 1;
+			hideLayer[i][j].bias_delta = 0.0;
+			for(k = 0; k< HIDE_NODE; k++)
+			{
+				//hideLayer[i][j].weight[k] = rand() % 10000 / (double)10000 * 2 - 1;
+				hideLayer[i][j].weight[k] = 1;
+				hideLayer[i][j].weight_delta[k] = 0.0;
+			}
 		}
 	}
 	
 	//hide init
 	for(i = 0; i< HIDE_NODE; i++)
 	{
-		hideLayer[i].weight = malloc(sizeof(double) * OUTPUT_NODE);
-		hideLayer[i].weight_delta = malloc(sizeof(double) * OUTPUT_NODE);
-		hideLayer[i].bias = rand() % 10000 / (double)10000 * 2 - 1;
-		hideLayer[i].bias_delta = 0.0;
+		hideLayer[i][HIDE_NODE_COL - 1].weight = malloc(sizeof(double) * OUTPUT_NODE);
+		hideLayer[i][HIDE_NODE_COL - 1].weight_delta = malloc(sizeof(double) * OUTPUT_NODE);
+		hideLayer[i][HIDE_NODE_COL - 1].bias = rand() % 10000 / (double)10000 * 2 - 1;
+		hideLayer[i][HIDE_NODE_COL - 1].bias_delta = 0.0;
 		for(j = 0; j< OUTPUT_NODE; j++)
 		{
-			hideLayer[i].weight[j] = rand() % 10000 / (double)10000 * 2 - 1;
-			hideLayer[i].weight_delta[j] = 0.0;
+			//hideLayer[i][HIDE_NODE_COL - 1].weight[j] = rand() % 10000 / (double)10000 * 2 - 1;
+			hideLayer[i][HIDE_NODE_COL - 1].weight[j] = 1;
+			hideLayer[i][HIDE_NODE_COL - 1].weight_delta[j] = 0.0;
 		}
 	}
 	
@@ -188,6 +208,7 @@ void resetDelta()
 {
 	int i;
 	int j;
+	int k;
 	for(i = 0; i < INPUT_NODE; i++)
 	{
 		for(j = 0; j < HIDE_NODE; j++)
@@ -196,12 +217,22 @@ void resetDelta()
 		}
 	}
 
+	for(i = 0; i< HIDE_NODE; i++)
+	{
+		for(j = 0; j< HIDE_NODE_COL - 1; j++)
+		{
+			hideLayer[i][j].bias_delta = 0.0;
+			for(k = 0; k< HIDE_NODE; k++)
+				hideLayer[i][j].weight_delta[k] = 0.0;
+		}
+	}
+
 	for(i = 0; i < HIDE_NODE; i++)
 	{
-		hideLayer[i].bias_delta = 0.0;
+		hideLayer[i][HIDE_NODE_COL - 1].bias_delta = 0.0;
 		for(j = 0; j < OUTPUT_NODE; j++)
 		{
-			hideLayer[i].weight_delta[j] = 0.0;
+			hideLayer[i][HIDE_NODE_COL - 1].weight_delta[j] = 0.0;
 		}
 	}
 
@@ -251,16 +282,31 @@ int main()
 					sum += inpuLayer[inputLayer_post].value * inpuLayer[inputLayer_post].weight[hidelayer_pos];
 				}
 
-				sum -= hideLayer[hidelayer_pos].bias;
-				hideLayer[hidelayer_pos].value = sigmoid(sum);
+				sum -= hideLayer[hidelayer_pos][0].bias;
+				hideLayer[hidelayer_pos][0].value = sigmoid(sum);
 			}
+
+			//forward spread hide -> hide 
+			int i,j,k;
+			for(j = 1; j< HIDE_NODE_COL; j++)
+			{
+				for(i = 0; i< HIDE_NODE; i++)
+				{
+					double sum = 0.0;
+					for(k = 0; k< HIDE_NODE; k++)
+						sum += hideLayer[k][j - 1].value * hideLayer[k][j - 1].weight[i];
+					sum -= hideLayer[i][j].bias;
+					hideLayer[i][j].value = sigmoid(sum);
+				}
+			}
+
 			//forward spread hide -> output
 			for(outputlayer_pos = 0; outputlayer_pos < OUTPUT_NODE; outputlayer_pos++)
 			{
 				double sum = 0.0;
 				for(hidelayer_pos= 0; hidelayer_pos < HIDE_NODE; hidelayer_pos++)
 				{
-					sum += hideLayer[hidelayer_pos].value * hideLayer[hidelayer_pos].weight[outputlayer_pos];
+					sum += hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].value * hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].weight[outputlayer_pos];
 				}
 
 				sum -= outputLayer[outputlayer_pos].bias;
@@ -291,14 +337,15 @@ int main()
 			{
 				double bias_delta = 1;
 
-				bias_delta *= d_loss(outputLayer[outputlayer_pos].loss_value);
-				bias_delta *= d_sigmoid(outputLayer[outputlayer_pos].value);
+				bias_delta *= d_loss(outputLayer[outputlayer_pos].loss_value); // a(loss) / a(y^) 
+				bias_delta *= d_sigmoid(outputLayer[outputlayer_pos].value);   // a(y^)   / a(I)
 				outputLayer[outputlayer_pos].d_param = bias_delta;
 
 				bias_delta *= d_bias();
 				bias_delta *= -1;
 				outputLayer[outputlayer_pos].bias_delta += bias_delta;
 			}
+
 			//backward spread output -> hide check hide weight
 			for(hidelayer_pos = 0; hidelayer_pos< HIDE_NODE; hidelayer_pos++)
 			{
@@ -306,27 +353,72 @@ int main()
 				{
 					double weight_delta = 1;
 					weight_delta *= outputLayer[outputlayer_pos].d_param;
-					weight_delta *= d_weight(hideLayer[hidelayer_pos].value);
+					weight_delta *= d_weight(hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].value);
 					weight_delta *= -1;
-					hideLayer[hidelayer_pos].weight_delta[outputlayer_pos] += weight_delta;
+					hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].weight_delta[outputlayer_pos] += weight_delta;
 				}
 			}
 			//backward spread output -> hide check hide bias 
 			for(hidelayer_pos = 0; hidelayer_pos< HIDE_NODE; hidelayer_pos++)
 			{
 				double sum_delta = 0;
+				double sum_param = 0;
+				double sum_all = 0;
+				for(k = 0; k < OUTPUT_NODE; k++)
+						sum_param += hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].weight[k];
 				for(outputlayer_pos= 0; outputlayer_pos< OUTPUT_NODE; outputlayer_pos++)
 				{
 				  double sum = 1;
 					sum *= outputLayer[outputlayer_pos].d_param;
-					sum *= d_value(hideLayer[hidelayer_pos].weight[outputlayer_pos]);
-					sum *= d_sigmoid(hideLayer[hidelayer_pos].value);
-					hideLayer[hidelayer_pos].d_param = sum;
+					sum *= d_value(hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].weight[outputlayer_pos]);
+					sum *= d_sigmoid(hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].value);
+					sum_all += sum * (hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].weight[outputlayer_pos] / sum_param);
 					sum *= d_bias();
 					sum_delta += sum;
 				}
+				hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].d_param = (sum_all);
 				sum_delta *= -1;
-				hideLayer[hidelayer_pos].bias_delta += sum_delta;
+				hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].bias_delta += sum_delta;
+			}
+		
+			if(HIDE_NODE_COL > 1)
+			{
+				for(j = HIDE_NODE_COL - 2; j >= 0; j--)
+				{
+					for(i = 0; i < HIDE_NODE; i++)
+					{
+						double sum_delta = 0;
+						double sum_param = 0;
+						double sum_all = 0;
+						for(k = 0; k < HIDE_NODE; k++)
+								sum_param += hideLayer[i][j].weight[k];
+
+						//backward spread hide -> hide check hide weight
+						for(k = 0; k < HIDE_NODE; k++)
+						{
+							double weight_delta = 1;
+							weight_delta *= hideLayer[k][j + 1].d_param;
+							weight_delta *= d_weight(hideLayer[i][j].value);
+							weight_delta *= -1;
+							hideLayer[i][j].weight_delta[k] += weight_delta;
+
+							//backward spread hide -> hide check hide bias 
+							{
+								double sum = 1;
+								sum *= hideLayer[k][j + 1].d_param;
+								sum *= d_value(hideLayer[i][j].weight[k]);
+								sum *= d_sigmoid(hideLayer[i][j].value);
+								sum_all += sum * (hideLayer[i][j].weight[k] / sum_param);
+								sum *= d_bias();
+								sum_delta += sum;
+							}
+						}
+					
+						hideLayer[i][j].d_param = sum_all;
+						sum_delta *= -1;
+						hideLayer[i][j].bias_delta += sum_delta;
+					}
+				}
 			}
 
 			//backward spread hide -> input check input weight
@@ -335,7 +427,7 @@ int main()
 				for(hidelayer_pos= 0; hidelayer_pos< HIDE_NODE; hidelayer_pos++)
 				{
 					double sum = 1;
-					sum *= hideLayer[hidelayer_pos].d_param;
+					sum *= hideLayer[hidelayer_pos][0].d_param;
 					sum *= d_weight(inpuLayer[inputLayer_post].value);
 					sum *= -1;
 					inpuLayer[inputLayer_post].weight_delta[hidelayer_pos] += sum;
@@ -347,6 +439,7 @@ int main()
 			printf("train complete, train:%d max error:%lf\n", trainTime + 1, error_max);
 			break;
 		}
+		printf("\r[%d]times:%d %lf %lf%%  error:%lf", currTrainSample_pos, trainTime, mostTimes, trainTime / mostTimes, error_max);
 
 		//modify
 		for(inputLayer_post = 0 ; inputLayer_post < INPUT_NODE; inputLayer_post++)
@@ -357,13 +450,17 @@ int main()
 					inpuLayer[inputLayer_post].weight_delta[hidelayer_pos] /(double) trainSize;
 			}
 		}
+		int j;
 		for(hidelayer_pos = 0 ; hidelayer_pos< HIDE_NODE; hidelayer_pos++)
 		{
-			hideLayer[hidelayer_pos].bias +=studyRate * hideLayer[hidelayer_pos].bias_delta / (double)trainSize;
-		  for(outputlayer_pos= 0 ; outputlayer_pos< OUTPUT_NODE; outputlayer_pos++)
-		  {
-				hideLayer[hidelayer_pos].weight[outputlayer_pos] += studyRate * 
-					hideLayer[hidelayer_pos].weight_delta[outputlayer_pos] /(double) trainSize;
+			for(j = 0 ; j < HIDE_NODE_COL; j++)
+			{
+				hideLayer[hidelayer_pos][j].bias +=studyRate * hideLayer[hidelayer_pos][j].bias_delta / (double)trainSize;
+				for(outputlayer_pos= 0 ; outputlayer_pos< OUTPUT_NODE; outputlayer_pos++)
+				{
+					hideLayer[hidelayer_pos][j].weight[outputlayer_pos] += studyRate * 
+						hideLayer[hidelayer_pos][j].weight_delta[outputlayer_pos] /(double) trainSize;
+				}
 			}
 		}
 		for(outputlayer_pos= 0 ; outputlayer_pos< OUTPUT_NODE; outputlayer_pos++)
@@ -386,15 +483,27 @@ int main()
 			{
 				sum += inpuLayer[inputLayer_post].value * inpuLayer[inputLayer_post].weight[hidelayer_pos];
 			}
-			sum -= hideLayer[hidelayer_pos].bias;
-			hideLayer[hidelayer_pos].value = sigmoid(sum);
+			sum -= hideLayer[hidelayer_pos][0].bias;
+			hideLayer[hidelayer_pos][0].value = sigmoid(sum);
 		}
+		int j,k;
+		for(j = 1; j < HIDE_NODE_COL; j++)
+			for(hidelayer_pos = 0; hidelayer_pos < HIDE_NODE; hidelayer_pos++)
+			{
+				double sum = 0.0;
+				for(k = 1; k < HIDE_NODE; k++)
+				{
+						sum += hideLayer[k][j - 1].value * hideLayer[k][j - 1].weight[hidelayer_pos];
+				}
+				sum -= hideLayer[hidelayer_pos][j].bias;
+				hideLayer[hidelayer_pos][j].value = sigmoid(sum);
+			}
 		for(outputlayer_pos= 0; outputlayer_pos< OUTPUT_NODE; outputlayer_pos++)
 		{
 			double sum = 0.0;
 			for(hidelayer_pos= 0; hidelayer_pos< HIDE_NODE; hidelayer_pos++)
 			{
-				sum += hideLayer[hidelayer_pos].value * hideLayer[hidelayer_pos].weight[outputlayer_pos];
+				sum += hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].value * hideLayer[hidelayer_pos][HIDE_NODE_COL - 1].weight[outputlayer_pos];
 			}
 			sum -= outputLayer[outputlayer_pos].bias;
 			outputLayer[outputlayer_pos].value = sigmoid(sum);
